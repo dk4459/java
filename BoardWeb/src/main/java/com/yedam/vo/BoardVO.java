16f474a14bcb18ba3f59437952d7c232c16257a5
Your Name <you@example.com>
2025-02-19 20:02:38 +0900

    1234

--- BoardWeb/src/main/java/com/yedam/vo/BoardVO.java
+++ BoardWeb/src/main/java/com/yedam/vo/BoardVO.java
@@ -2,15 +2,15 @@ package com.yedam.vo;
 
 import java.util.Date;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
 
 
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
 
 public class BoardVO {
 	private int boardNo; // boat
@@ -18,5 +18,43 @@ public class BoardVO {
 	private String content;
 	private String writer;
 	private Date writerDate;
-	private int viewCnt;	
+	private int viewCnt;
+	public int getBoardNo() {
+		return boardNo;
+	}
+	public void setBoardNo(int boardNo) {
+		this.boardNo = boardNo;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public Date getWriterDate() {
+		return writerDate;
+	}
+	public void setWriterDate(Date writerDate) {
+		this.writerDate = writerDate;
+	}
+	public int getViewCnt() {
+		return viewCnt;
+	}
+	public void setViewCnt(int viewCnt) {
+		this.viewCnt = viewCnt;
+	}	
+	
+	
 }
